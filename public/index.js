62a4248043b4b90c91f2d136262cb7c7e125a8a3
Ahmad Hudori <ahmadhudori06@gmail.com>
2024-03-19 11:56:41 +0700

    menambahkan fitur dark mode dan devdependecy prettier dan juga prettier-plugin-tailwindcss

--- public/index.js
+++ public/index.js
@@ -1,12 +1,19 @@
+console.log("hai");
+
 // Navbar Fixed
 window.onscroll = function () {
     const header = document.querySelector("header");
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector("#to-top");
 
     if (window.scrollY > fixedNav) {
         header.classList.add("navbar-fixed");
+        toTop.classList.add("flex");
+        toTop.classList.remove("hidden");
     } else {
         header.classList.remove("navbar-fixed");
+        toTop.classList.remove("flex");
+        toTop.classList.add("hidden");
     }
 };
 
@@ -19,4 +26,22 @@ hamburger.addEventListener("click", function () {
     navMenu.classList.toggle("hidden");
 });
 
-console.log("hai");
+window.addEventListener("click", function (e) {
+    if (e.target != navMenu && e.target != hamburger) {
+        navMenu.classList.add("hidden");
+        hamburger.classList.remove("hamburger-active");
+    }
+});
+
+// Dark  Mode
+
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+    if (darkToggle.checked) {
+        html.classList.add("dark");
+    } else {
+        html.classList.remove("dark");
+    }
+});
